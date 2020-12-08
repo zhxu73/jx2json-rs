612675b6b4ec3b286bc21ee77400f129c7934a69
John Xu <zhxu73@email.arizona.edu>
2020-12-07 21:08:05 -0700

    read filename from cmd line args

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod jx_token;
 mod scanner;
 
+use std::{env, process::exit};
+
 fn main() {
-    let filename = String::from("test.jx");
+    let filename = parse_args();
     let result = scanner::scan_file(&filename);
 
     match result {
@@ -13,4 +15,26 @@ fn main() {
         }
         Err(err) => println!("{}", err),
     }
+}
+
+fn parse_args() -> String {
+    let args: Vec<String> = env::args().collect();
+    if args[1..].is_empty() {
+        eprintln!("no args supplied");
+        print_help();
+        exit(-1)
+    }
+    for arg in &args[1..] {
+        match arg.as_str() {
+            "--help" | "-h" => print_help(),
+            // filename
+            _ => return arg.clone(),
+        };
+    }
+    eprintln!("needs filename");
+    exit(-1);
+}
+
+fn print_help() {
+    println!("jx2json [--help|-h] [jx-filename]");
 }
